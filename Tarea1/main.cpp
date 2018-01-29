2ca770970ed380dd1c3c467d9bc4e32f60dfd255
jufergom <jufergom@hotmail.com>
2018-01-29 17:28:58 -0600

    Agregada tarea 1 de 3 algoritmos

---
+++ Tarea1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+//ordenar arreglo de menor a mayor
+int* bubblesort(int[] intArray, int arraySize) {
+    int temp;
+    for(int i = 0; i < arraySize - 1, i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(intArray[j] > intArray[j+1]) {
+                temp = intArray[j];
+                intArray[j] = intArray[j+1];
+                intArray[j+1] = temp;
+            }
+        }
+    }
+}
+
+//
+int factorial(int x) {
+    if(x == 0)
+        return 1;
+    return x * factorial(x-1);
+}
+
+bool checkWord(string word1, string word2) {
+    if(word1.size() > word2.size())
+        return false;
+    else {
+
+    }
+}
+
+int main() {
+
+    return 0;
+}
+
+
